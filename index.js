e3c6c2bd6711238fb46cef8f3d34c9a43fb16776
karolbilicki <karoolbilicki@gmail.com>
2016-07-28 17:51:44 +0200

    compact

--- index.js
+++ index.js
@@ -21,5 +21,22 @@ describe('lodash tests', function(){
 			expected = [[1,2,3,4,5]];
 			assert.equal(JSON.stringify(current), JSON.stringify(expected));
 		});
+		it('compact-1', function(){
+			current = _.compact([1,2,3,4,5,null,0,NaN,undefined,""]);
+			expected = [1,2,3,4,5];
+			assert.equal(JSON.stringify(current), JSON.stringify(expected));
+		});
+		it('compact-2', function(){
+			current = _.compact([null,0,NaN,undefined,""]);
+			expected = [];
+			assert.equal(JSON.stringify(current), JSON.stringify(expected));
+		});
+		it('compact-3', function(){
+			current = _.compact([["",0,null],null,0,NaN,undefined,""]);
+			expected = [["",0,null]];
+			assert.equal(JSON.stringify(current), JSON.stringify(expected));
+		});
+		it('', function(){
+		});
 	});
 });
